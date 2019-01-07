7da2bc12faeb29257953023521239e3dcde62dcc
yuzhigang <yuzhigang6687116@163.com>
2019-01-07 17:03:42 +0800

    提交基础代码

---
+++ src/main/java/com/linglu/biz/info/InfoApplication.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018 linglucloud.com All right reserved. This software is the
+ * confidential and proprietary information of linglucloud.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with linglucloud.com.
+ */
+
+package com.linglu.biz.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类InfoApplication的实现描述：灵鹿社区核心资讯库项目启动类
+ *
+ * @author yuzhigang
+ * @see 2019年1月1日 上午11:49:59
+ */
+@SpringBootApplication
+public class InfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoApplication.class, args);
+    }
+
+}
